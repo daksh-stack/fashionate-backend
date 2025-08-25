32506830202c296e96969e3948366581940fee0c
daksh <dakshhardiya34@gmail.com>
2025-08-25 07:28:46 +0530

    Project completed

--- src/index.js
+++ src/index.js
@@ -17,9 +17,15 @@ const allowedOrigins = [
 
 app.use(cookieParser())
 app.use(cors({
-    origin:allowedOrigins ,
-    credentials: true,
-}))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json({ limit: "5mb" }))
 app.use(express.json());
